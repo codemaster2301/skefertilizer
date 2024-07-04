6ac21e8dce0d8f1726e24fa280b3ea425f258206
codemaster2301 <abhaysurya2301@gmail.com>
2024-07-04 10:16:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Function to open WhatsApp with pre-filled message
+function sendMessage() {
+    var url = 'https://api.whatsapp.com/send?phone=+919482379110&text=hi';
+    window.open(url, '_blank');
+}
